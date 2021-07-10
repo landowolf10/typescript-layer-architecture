fbb8f7e8fac71473eba8ee3e99f80b374a2dad71
Orlando Avila <oa1220@dexcom.com>
2021-07-09 19:20:37 -0500

    app file configuration with class.

--- src/app.ts
+++ src/app.ts
@@ -10,26 +10,6 @@ import { userRouter } from './routes/user.routes';
 import { loginRouter } from './routes/login.routes';
 import { noteRouter } from './routes/note.routes';
 
-/*const app = express();
-const UseRoutes = express.Router();
-const loginRoutes = express.Router();
-const noteRoutes = express.Router();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use('/user', UseRoutes);
-app.use('/user/login', loginRoutes);
-app.use('/note', noteRoutes);
-
-userRouter(UseRoutes, new UserService(new UserDA()));
-loginRouter(loginRoutes, new LoginService(new LoginDA()));
-noteRouter(noteRoutes, new NoteService(new NoteDA()));
-
-app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT}`);
-});*/
-
 export class App
 {
     private app: Application;
